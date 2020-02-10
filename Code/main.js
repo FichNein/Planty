5a99725244d588fde84fc7b2ffc4995fe005a59a
feingon <feingon@wwu.edu>
2020-02-09 21:49:03 -0800

    'Working' buttons

---
+++ Code/main.js
@@ -0,0 +1,40 @@
+
+document.getElementById("prev").addEventListener("click", function() {
+    let picture = document.getElementById("plant");
+
+    let attr = picture.attributes[2].value
+    let regex = attr[10]
+
+    let i = 11
+   
+    while(attr[i] != '.') {
+        regex += attr[i];
+        i++;
+    }
+
+    if(regex > 1) {
+        regex--;
+        document.getElementById("plant").src="../photos/" + regex + ".jpg";
+        document.getElementById("desc").innerHTML = "Photo " + regex;
+    }
+
+});  
+
+document.getElementById("next").addEventListener("click", function() {
+    let picture = document.getElementById("plant");
+
+    let attr = picture.attributes[2].value
+    let regex = attr[10]
+
+    let i = 11
+   
+    while(attr[i] != '.') {
+        regex += attr[i];
+        i++;
+    }
+
+    regex++;
+    document.getElementById("plant").src="../photos/" + regex + ".jpg";
+    document.getElementById("desc").innerHTML = "Photo " + regex;
+    
+});  
