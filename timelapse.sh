01e505c8f063968c42f2621df90a7bdd3fbe6515
FichNein <feingon@wwu.edu>
2020-02-10 17:47:48 -0800

    updated timelapse to auto-push

--- timelapse.sh
+++ timelapse.sh
@@ -6,5 +6,9 @@ while true; do
   gphoto2 --capture-image-and-download --filename ./photos/"$CFN".jpg
   CFN="$((CFN + 1))"
   echo "$CFN" > ./currentFileName.txt
+  git pull
+  git add ~/timelapse/planty.com/photos/*.jpg
+  git commit -m "Updated: `date +'%Y-%m-%d %H:%M:%S'`"
+  git push
   sleep 2h 
 done
